c9874adba35ee9c262854da192b3e383899a96cc
GAURAV DUBEY <i.gauravdubey@gmail.com>
2025-02-06 03:51:00 +0530

    Add files via upload

--- src/dsatopics/strings/LeetCodeStringExamples.java
+++ src/dsatopics/strings/LeetCodeStringExamples.java
@@ -1,4 +1,4 @@
-package dsatopics.strings;
+package com.gaurav.dsalib.strings;
 
 import java.util.HashMap;
 
@@ -7,7 +7,8 @@ public class LeetCodeStringExamples {
     public static void main(String[] args) {
 
 //        System.out.println("\n  Isomorphic String : "+isIsomorphic("egg","adh"));
-        System.out.println(" Word Pattern : "+wordPattern("abba","dog cat cat dog"));
+//        System.out.println(" Word Pattern : "+wordPattern("abba","dog cat cat dog"));
+        System.out.println(" Anagram String : "+isAnagram("anagram","nagaram"));
     }
 
     /**
@@ -75,4 +76,46 @@ public class LeetCodeStringExamples {
         }
         return true;
     }
+
+
+    /**
+     * Check whether string is Anagram or not
+     * @param s
+     * @param t
+     * @return
+     */
+    private static boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer>mapOne=new HashMap<>();
+        HashMap<Character,Integer>mapTwo=new HashMap<>();
+        for (int i=0;i<s.length();i++) {
+            char char1 = s.charAt(i);
+            char char2 = t.charAt(i);
+            mapOne.put(char1,mapOne.getOrDefault(char1,0)+1);
+            mapTwo.put(char2,mapTwo.getOrDefault(char2,0)+1);
+        }
+        return mapOne.equals(mapTwo);
+    }
+
+    /**
+     * Reverse of word in String
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        // Trim leading/trailing spaces & split by multiple spaces
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        // Iterate in reverse order
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i > 0) result.append(" "); // Add space except for last word
+        }
+
+        return result.toString();
+    }
+
 }
